825b301360f4de6cc9140f47fe4697f546cecd28
Masroor73 <miamasroor.ahamed@edu.sait.ca>
2025-01-28 21:03:52 -0700

    Commit for week 2 assignment

---
+++ app/week-2/student-info.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+export default function StudentInfo() {
+    return (
+        <div>
+            <p>Mia Masroor Ahamed</p>
+            <Link href="https://github.com/Masroor73/cprg306-assignments">
+                View My GitHub Repository
+            </Link>
+        </div>
+    );
+}
